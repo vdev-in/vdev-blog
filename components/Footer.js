8f8a0152e872e1143372b3e1730b797c9218b2f6
vimalverma558 <kumarvimal558@gmail.com>
2021-06-10 10:36:40 +0530

    seo tags improved 🚀

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,3 @@
-import styles from '../styles/footer.module.css'
-
 export default function Footer() {
   return (
     <div>
--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,3 @@
-import styles from '../styles/header.module.css'
-
 export default function Header() {
   return (
     <div>
